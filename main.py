2bf7d23909709a413a3101309f5a73b53f54919e
Luckich000 <maksimrakultcev002@gmail.com>
2025-03-19 00:46:24 +0500

    обновка

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ from psycopg2 import sql
 def load(id):
     e = cursor.execute('SELECT * from rakultcev_vagel.car WHERE car_id = %s',(id,))
     print(cursor.fetchall())
+    e = cursor.execute('SELECT * from rakultcev_vagel.accident WHERE car_id = %s', (id,))
+    print(cursor.fetchall())
     # print(type(cursor.fetchall()))
-    # itog = json.dumps(cursor.fetchall()) ------не удавшийся эксперимент
+    # itog = json.dumps(cursor.fetchall()) ------неудавшийся эксперимент
     # print(itog)
 
-def save(id,model,year,color,number,type ):
+def save_car(id,model,year,color,number,type ):
     cursor.execute('INSERT INTO rakultcev_vagel.car VALUES (%s, %s, %s, %s, %s, %s)', (id,model, year,color,number,type))
+    conn.commit()
+
+def save_accident(accident_id,car_id,accident_date,description):
+    cursor.execute('INSERT INTO rakultcev_vagel.accident VALUES (%s, %s, %s, %s)',(accident_id, car_id, accident_date, description))
     conn.commit()
 
 def delete(id):
@@ -44,26 +50,28 @@ with conn.cursor() as cursor:
             print(f"Executed: {e}")
     conn.commit()
 
-    a = input("что вы хотите сделать? delete(удалить) load(показать информацию) save(сохранить информацию)")
-    if a == "delete":
-        delete(input("укажите id машины, которую хотите удалить:"))
-    else:
-        if a == "load":
-            load(input("Укажите id машины:"))
+    # a = input("что вы хотите сделать? delete(удалить) load(показать информацию) save(сохранить информацию)")
+    # if a == "delete":
+    #     delete(input("укажите id машины, которую хотите удалить:"))
+    # else:
+    #     if a == "load":
+    #         load(input("Укажите id машины:"))
+    #
+    #     else:
+    #         if a == "save":
+    #             save_car(input("id , модель, год, цвет, номер машины, тип машины:" ), input(), input(),input(),input(),input())
 
-        else:
-            if a == "save":
-                save(input("id , модель, год, цвет, номер машины, тип машины:" ), input(), input(),input(),input(),input())
 
     # если что-то пойдет не так:
+    # save_accident(input(),input(),'input(),input())
     # load(input())
     # delete(input())
-    # save(input(),input(),input(),input(),input(),input())
+    # save_car(input(),input(),input(),input(),input(),input())
 
 
     # дз:
     # load(1)
-    # save(4,'supra',2000,'red','943hg','sedan')
+    # save_car(4,'supra',2000,'red','943hg','sedan')
     # load(4)
     # delete(4)
 
