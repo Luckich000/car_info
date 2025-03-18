c465cf3b2bb8d1915bc667a9e2e94fa671108398
Luckich000 <maksimrakultcev002@gmail.com>
2025-03-18 18:51:09 +0500

    обновка

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import psycopg2
+import json
 from psycopg2 import sql
+
+def load(id):
+    e = cursor.execute('SELECT * from rakultcev_vagel.car WHERE car_id = %s',(id,))
+    print(cursor.fetchall())
+    # print(type(cursor.fetchall()))
+    # itog = json.dumps(cursor.fetchall()) ------не удавшийся эксперимент
+    # print(itog)
+
+def save(id,model,year,color,number,type ):
+    cursor.execute('INSERT INTO rakultcev_vagel.car VALUES (%s, %s, %s, %s, %s, %s)', (id,model, year,color,number,type))
+    conn.commit()
+
+def delete(id):
+    cursor.execute('DELETE FROM rakultcev_vagel.car WHERE car_id = %s',(id,))
+    conn.commit()
+
 
 host = '79.174.88.238'
 port = 15221
@@ -23,8 +40,27 @@ with open('commands.sql') as f:
 with conn.cursor() as cursor:
     for st in migrations.split(';'):
         if st.strip():
-            cursor.execute(st.strip())
-            print(f"Executed: {st.strip()}")
+            e = cursor.execute(st.strip())
+            print(f"Executed: {e}")
+    conn.commit()
+
+    a = input("что вы хотите сделать? delete(удалить) load(показать информацию) save(сохранить информацию)")
+    if a == "delete":
+        delete(input("укажите id машины, которую хотите удалить:"))
+    else:
+        if a == "load":
+            load(input("Укажите id машины:"))
+
+        else:
+            if a == "save":
+                save(input("id , модель, год, цвет, номер машины, тип машины:" ), input(), input(),input(),input(),input())
+
+    # если что-то пойдет не так:
+    # load(input())
+    # delete(input())
+    # save(input(),input(),input(),input(),input(),input())
+
+
 
 if __name__ == '__main__' :
     print('GG WP')
