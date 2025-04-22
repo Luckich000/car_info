71da6be3ff99676e4dfd5066d90e488d2be4b483
Luckich000 <maksimrakultcev002@gmail.com>
2025-04-22 20:53:55 +0500

    ОНО РАБОТАЕТ

--- main.py
+++ main.py
@@ -1,79 +1,41 @@
 import psycopg2
 import json
+import uvicorn
+from fastapi import FastAPI, APIRouter
 from psycopg2 import sql
+router = APIRouter()
+app=FastAPI()
+from server import router
 
-def load(id):
-    e = cursor.execute('SELECT * from rakultcev_vagel.car WHERE car_id = %s',(id,))
-    print(cursor.fetchall())
-    e = cursor.execute('SELECT * from rakultcev_vagel.accident WHERE car_id = %s', (id,))
-    print(cursor.fetchall())
-    # print(type(cursor.fetchall()))
-    # itog = json.dumps(cursor.fetchall()) ------неудавшийся эксперимент
-    # print(itog)
+DB_HOST = '79.174.88.238'
+DB_PORT = 15221
+DB_NAME = 'school_db'
+DB_USER = 'school'
+DB_PASSWORD = 'School1234*'
 
-def save_car(id,model,year,color,number,type ):
-    cursor.execute('INSERT INTO rakultcev_vagel.car VALUES (%s, %s, %s, %s, %s, %s)', (id,model, year,color,number,type))
-    conn.commit()
-
-def save_accident(accident_id,car_id,accident_date,description):
-    cursor.execute('INSERT INTO rakultcev_vagel.accident VALUES (%s, %s, %s, %s)',(accident_id, car_id, accident_date, description))
-    conn.commit()
-
-def delete(id):
-    cursor.execute('DELETE FROM rakultcev_vagel.car WHERE car_id = %s',(id,))
-    conn.commit()
-
-
-host = '79.174.88.238'
-port = 15221
-db_name = 'school_db'
-user = 'school'
-password = 'School1234*'
-
-
-conn = psycopg2.connect(
-    dbname=db_name,
-    host=host,
-    port=port,
-    user=user,
-    password=password
+connect = psycopg2.connect(
+    dbname=DB_NAME,
+    host = DB_HOST,
+    port = DB_PORT,
+    user = DB_USER,
+    password = DB_PASSWORD
 )
 
-migrations = ''
-with open('commands.sql') as f:
-    migrations = f.read()
 
-with conn.cursor() as cursor:
-    for st in migrations.split(';'):
-        if st.strip():
-            e = cursor.execute(st.strip())
-            print(f"Executed: {e}")
-    conn.commit()
+if __name__ == '__main__':
 
-    # a = input("что вы хотите сделать? delete(удалить) load(показать информацию) save(сохранить информацию)")
-    # if a == "delete":
-    #     delete(input("укажите id машины, которую хотите удалить:"))
-    # else:
-    #     if a == "load":
-    #         load(input("Укажите id машины:"))
-    #
-    #     else:
-    #         if a == "save":
-    #             save_car(input("id , модель, год, цвет, номер машины, тип машины:" ), input(), input(),input(),input(),input())
+    migrations = ''
+    with open('commands.sql') as f:
+        migrations = f.read()
 
-
-    # если что-то пойдет не так:
-    # save_accident(input(),input(),'input(),input())
-    # load(input())
-    # delete(input())
-    # save_car(input(),input(),input(),input(),input(),input())
-
-
-    # дз:
-    # load(1)
-    # save_car(4,'supra',2000,'red','943hg','sedan')
-    # load(4)
-    # delete(4)
-
-if __name__ == '__main__' :
+    with connect.cursor() as cursor:
+        for st in migrations.split(';'):
+            if st.strip():
+                e = cursor.execute(st.strip())
+                print(f"Executed: {e}")
+        connect.commit()
     print('GG WP')
+    app.include_router(router)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
+
---
+++ server.py
@@ -0,0 +1,78 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from random import randint
+import psycopg2
+from psycopg2 import sql
+router = APIRouter()
+
+DB_HOST = '79.174.88.238'
+DB_PORT = 15221
+DB_NAME = 'school_db'
+DB_USER = 'school'
+DB_PASSWORD = 'School1234*'
+
+conn = psycopg2.connect(
+    dbname=DB_NAME,
+    host = DB_HOST,
+    port = DB_PORT,
+    user = DB_USER,
+    password = DB_PASSWORD
+)
+
+
+class Accidents(BaseModel):
+    car_id: int
+    accident_date: str
+    description: str
+
+class Cars(BaseModel):
+    model: str
+    car_year: int
+    color: str
+    car_number: str
+    car_type : str
+
+
+@router.post("/car/create")
+async def add_car(car: Cars):
+    cur = conn.cursor()
+    id = randint(1,99999999)
+    cur.execute(
+        'INSERT INTO rakultcev_vagel.car VALUES (%s, %s, %s, %s, %s, %s)',
+        (id,car.model, car.car_year,car.color,car.car_number,car.car_type))
+    conn.commit()
+    text = "ДОБАВЛЕНО. ваш id - {id}"
+    itog = text.format(id = id)
+    return itog
+
+# @router.post("/accident/add/{id}")
+# async def add_accident(id, acci:Accidents):
+#     cur = conn.cursor()
+#     accident_id = randint(1,9999999999999)
+#     cur.execute(
+#         'INSERT INTO rakultcev_vagel.accident VALUES (%s, %s, %s, %s)',
+#         (accident_id, id, acci.accident_date, acci.description))
+#     conn.commit()
+#     return "ДОБАВЛЕНО"
+
+@router.delete("/car/delete/{id}")
+async  def delete_car(id):
+    cur = conn.cursor()
+    cur.execute(
+        'DELETE FROM rakultcev_vagel.car WHERE car_id = %s',
+        (id,))
+    conn.commit()
+    return "УДАЛЕНО"
+
+@router.get("/car/{id}")
+async def get_car(id):
+    cur = conn.cursor()
+    cur.execute(
+        'SELECT * from rakultcev_vagel.car WHERE car_id = %s',
+        (id,))
+    itog1 = cur.fetchall()
+    cur.execute(
+        'SELECT * from rakultcev_vagel.accident WHERE car_id = %s',
+        (id,))
+    itog2 = cur.fetchall()
+    return itog1 + itog2
